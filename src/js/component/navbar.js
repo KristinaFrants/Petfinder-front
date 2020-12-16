de2ff736f4248d025601c8f425cda74b998c79c9
Kristina <cristina.chiticari@gmail.com>
2020-12-16 15:16:02 -0500

    Update navbar.js

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -13,70 +13,67 @@ import { Context } from "../store/appContext";
 export function Navibar() {
 	const { store, actions } = useContext(Context);
 	return (
-		<Navbar variant="success" className="mb-2 navdesign">
-			<Navbar.Brand className="ourfont2">
-				<Link style={{ color: "black", textDecoration: " overline dotted grey", marginLeft: "4%" }} to="/">
-					{" "}
+		<Navbar bg="light" expand="lg">
+			<Navbar.Brand className="">
+				<Link style={{ color: "black" }} to="/">
 					Petfinders
 				</Link>
 			</Navbar.Brand>
-			<Nav className="mr-auto">
-				{/* <Link className="navhovering ourfont" to="/">
-					Home
-				</Link> */}
-				<Link className="navhovering ourfont" to="/feed">
-					Feed
-				</Link>
-				{store.contacts != null || store.contacts != undefined ? (
-					<Link className="navhovering ourfont" to="/dashboard-b">
-						Dashboard
-					</Link>
-				) : null}
-				<Link className="navhovering ourfont" to="/blog">
-					Blog
-				</Link>
-				{store.contacts == null || store.contacts == undefined ? (
-					<Button className="btn-danger bg-danger ourbtn ml-2" variant="outline-light">
-						<Link style={{ color: "black", textDecoration: "none" }} to="/alertMsg">
-							Quick Alert
-						</Link>
-						<i className="fas fa-exclamation-circle ml-2" />
-					</Button>
-				) : null}
-			</Nav>
-			{/* <Form inline className="p-2 mr-1">
-				<FormControl type="text" placeholder="Search" className="mr-sm-2" />
-				<Button variant="outline-light">
-					<i className="fas fa-search" />
-				</Button>
-			</Form> */}
 
-			<NavDropdown
-				variant="dark"
-				bg="dark"
-				alignRight
-				className="wrapper p-2 mr-5"
-				title={<i style={{ color: "black" }} className="far fa-user" />}
-				drop="down">
-				<NavDropdown.Item>
-					<Link style={{ textDecoration: "none", color: "black" }} to="/signup">
-						Sign up
+			<Navbar.Toggle aria-controls="basic-navbar-nav" />
+			<Navbar.Collapse id="basic-navbar-nav">
+				<Nav className="mr-auto">
+					<NavDropdown
+						variant="dark"
+						bg="dark"
+						alignRight
+						className="wrapper p-2 ml-3"
+						title={<i style={{ color: "black" }} className="far fa-user" />}
+						drop="down">
+						<NavDropdown.Item>
+							<Link style={{ textDecoration: "none", color: "black" }} to="/signup">
+								Sign up
+							</Link>
+						</NavDropdown.Item>
+						{store.contacts != null || store.contacts != undefined ? (
+							<NavDropdown.Item onClick={() => actions.logout()}>
+								<Link style={{ textDecoration: "none", color: "black" }} to="/">
+									Logout
+								</Link>
+							</NavDropdown.Item>
+						) : (
+							<NavDropdown.Item>
+								<Link style={{ textDecoration: "none", color: "black" }} to="/login">
+									Login
+								</Link>
+							</NavDropdown.Item>
+						)}
+					</NavDropdown>
+
+					<Link className="navhovering ourfont" to="/feed">
+						Feed
 					</Link>
-				</NavDropdown.Item>
-				{store.contacts != null || store.contacts != undefined ? (
-					<NavDropdown.Item onClick={() => actions.logout()}>
-						<Link style={{ textDecoration: "none", color: "black" }} to="/">
-							Logout
+					{store.contacts != null || store.contacts != undefined ? (
+						<Link className="navhovering ourfont" to="/dashboard-b">
+							Dashboard
 						</Link>
-					</NavDropdown.Item>
-				) : (
-					<NavDropdown.Item>
-						<Link style={{ textDecoration: "none", color: "black" }} to="/login">
-							Login
-						</Link>
-					</NavDropdown.Item>
-				)}
-			</NavDropdown>
+					) : null}
+
+					<Link className="navhovering ourfont" to="/blog">
+						Blog
+					</Link>
+				</Nav>
+				<Nav>
+					{store.contacts == null || store.contacts == undefined ? (
+						<Button className="mr-3" variant="dark" id="btnAlert">
+							<Link style={{ color: "white", textDecoration: "none" }} to="/alertMsg">
+								Quick Add
+							</Link>
+							<i className="fas fa-exclamation-circle ml-2" />
+						</Button>
+					) : null}
+				</Nav>
+			</Navbar.Collapse>
 		</Navbar>
 	);
 }
